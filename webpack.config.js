367fd2a45a9c446dfdb75b81e0e92fd1514a929e
AB <am.id.dqd@gmail.com>
2018-04-12 21:22:58 +0300

    webpack config fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-   template: __dirname + '/src/index.html',
+   template: path.resolve(__dirname, 'src/index.html'),
    filename: 'index.html',
    inject: 'body'
 });
 
 module.exports = {
-   entry: ['./src/index.js'],
+   entry: path.resolve(__dirname, 'src/index.js'),
    output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'build.js'
@@ -17,8 +17,8 @@ module.exports = {
       rules: [
          {
             test: /\.js$/,
-            include: [path.resolve(__dirname, 'src')],
-            exclude: [path.resolve(__dirname, 'node_modules')],
+            include: path.resolve(__dirname, 'src'),
+            exclude: path.resolve(__dirname, 'node_modules'),
             loader: 'babel-loader'
          },
          {  test: /\.css$/,
